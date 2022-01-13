ed07ccc0941d02e42e54853a848d0bfe461c8704
Flyingblu <realflyingblu@gmail.com>
2022-01-13 21:06:33 +0800

    [Auth] Implement the production auth

--- src/main/java/io/xdea/xmux/forum/service/UserService.java
+++ src/main/java/io/xdea/xmux/forum/service/UserService.java
@@ -4,8 +4,8 @@ public interface UserService {
     public AuthResult auth(String token);
 
     public static class AuthResult {
-        public String uid;
-        public boolean success;
+        final public String uid;
+        final public boolean success;
 
         public AuthResult(String uid, boolean success) {
             this.uid = uid;
--- src/main/java/io/xdea/xmux/forum/service/impl/XmuxUserServiceImpl.java
+++ src/main/java/io/xdea/xmux/forum/service/impl/XmuxUserServiceImpl.java
@@ -1,16 +1,22 @@
 package io.xdea.xmux.forum.service.impl;
 
 import io.xdea.xmux.forum.service.UserService;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
+import xmux.core.auth.v4.AuthGrpc;
+import xmux.core.auth.v4.AuthOuterClass;
 
 @Service
 @Profile("prod")
 public class XmuxUserServiceImpl implements UserService {
+    @GrpcClient(AuthGrpc.SERVICE_NAME)
+    private AuthGrpc.AuthBlockingStub authBlockingStub;
 
     @Override
     public AuthResult auth(String token) {
-        System.out.println("TODO: Implement io.xdea.xmux.forum.service.impl.XmuxUserServiceImpl! ");
-        return null;
+        final var authUserResp = authBlockingStub.authUser(AuthOuterClass.AuthUserReq
+                .newBuilder().setFirebaseIdToken(token).build());
+        return new AuthResult(authUserResp.getUid(), true);
     }
 }
