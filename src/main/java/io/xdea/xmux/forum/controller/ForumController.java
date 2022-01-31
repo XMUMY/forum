67ffb5c5dad02d08da976de62a6fa08804a61d5f
Flyingblu <realflyingblu@gmail.com>
2022-01-31 17:25:14 +0800

    Enable Sentry performance tracking on controller

--- src/main/java/io/xdea/xmux/forum/controller/ForumController.java
+++ src/main/java/io/xdea/xmux/forum/controller/ForumController.java
@@ -1,10 +1,12 @@
 package io.xdea.xmux.forum.controller;
 
+import io.sentry.spring.tracing.SentryTransaction;
 import io.xdea.xmux.forum.service.*;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @GrpcService
+@SentryTransaction(operation = "controller")
 public class ForumController extends SavedController {
     @Autowired
     public ForumController(GroupService groupService, NotifService notifService, PostService postService,
