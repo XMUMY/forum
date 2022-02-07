409e1ad32fdb8d76684c9d8578a67f6c88341c01
Flyingblu <realflyingblu@gmail.com>
2022-02-07 23:10:06 +0800

    Fix basic auth

--- src/main/java/io/xdea/xmux/forum/service/impl/XmuxUserServiceImpl.java
+++ src/main/java/io/xdea/xmux/forum/service/impl/XmuxUserServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 import xmux.core.auth.v4.AuthGrpc;
 import xmux.core.auth.v4.AuthOuterClass;
 
+import java.util.Base64;
+
 @Service
 @Profile("prod")
 public class XmuxUserServiceImpl implements UserService {
@@ -15,8 +17,21 @@ public class XmuxUserServiceImpl implements UserService {
 
     @Override
     public AuthResult auth(String token) {
+        if (token == null || !token.startsWith("basic ")) {
+            return new AuthResult(null, false);
+        }
+        token = token.substring("basic ".length()).trim();
+        byte[] decodedToken = Base64.getDecoder().decode(token);
+        String[] userInfo = new String(decodedToken).split(":");
+        if (userInfo.length != 2) {
+            return new AuthResult(null, false);
+        }
         final var authUserResp = authBlockingStub.authUser(AuthOuterClass.AuthUserReq
-                .newBuilder().setFirebaseIdToken(token).build());
+                .newBuilder().setCampusIdPassword(
+                        AuthOuterClass.CampusIdPasswordCredential.newBuilder()
+                                .setCampusId(userInfo[0])
+                                .setPassword(userInfo[1]))
+                .build());
         return new AuthResult(authUserResp.getUid(), true);
     }
 }
