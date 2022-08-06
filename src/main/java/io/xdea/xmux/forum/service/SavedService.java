38af6be8c0ca085d0b8e99ccbf1ed107c5b8ab8f
Flyingblu <realflyingblu@gmail.com>
2022-08-06 13:53:29 +0800

    Add Redis for caching

--- src/main/java/io/xdea/xmux/forum/service/SavedService.java
+++ src/main/java/io/xdea/xmux/forum/service/SavedService.java
@@ -7,25 +7,51 @@ import io.xdea.xmux.forum.model.SavedPostExample;
 import io.xdea.xmux.forum.model.SavedThread;
 import io.xdea.xmux.forum.model.SavedThreadExample;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SavedService {
     final private SavedPostMapper savedPostMapper;
     final private SavedThreadMapper savedThreadMapper;
+    final private RedisTemplate<String, String> redisTemplate;
 
     @Autowired
-    public SavedService(SavedPostMapper savedPostMapper, SavedThreadMapper savedThreadMapper) {
+    public SavedService(SavedPostMapper savedPostMapper, SavedThreadMapper savedThreadMapper, RedisTemplate<String, String> redisTemplate) {
         this.savedPostMapper = savedPostMapper;
         this.savedThreadMapper = savedThreadMapper;
+        this.redisTemplate = redisTemplate;
     }
 
-    public boolean checkThreadSaved(String uid, int threadId) {
+    private String savedThreadKey(String uid) {
+        return uid + ":strd";
+    }
+
+    private void checkAndLoadSavedThreadCache(String uid) {
+        String key = savedThreadKey(uid);
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(key)))
+            return;
+
         SavedThreadExample savedThreadExample = new SavedThreadExample();
-        savedThreadExample.createCriteria().andUidEqualTo(uid).andThreadIdEqualTo(threadId);
-        return savedThreadMapper.countByExample(savedThreadExample) >= 1;
+        savedThreadExample.createCriteria().andUidEqualTo(uid);
+        List<SavedThread> trds = savedThreadMapper.selectByExample(savedThreadExample);
+        String[] trdIds = new String[trds.size()];
+        for (int i = 0; i < trds.size(); i++) {
+            trdIds[i] = trds.get(i).getThreadId().toString();
+        }
+        if (trdIds.length != 0) {
+            redisTemplate.opsForSet().add(key, trdIds);
+            redisTemplate.expire(key, 3650, TimeUnit.DAYS);
+        }
+    }
+
+    public boolean checkThreadSaved(String uid, Integer threadId) {
+        checkAndLoadSavedThreadCache(uid);
+        return redisTemplate.opsForSet().isMember(savedThreadKey(uid), threadId.toString());
     }
 
     public boolean checkPostSaved(String uid, int postId) {
@@ -34,7 +60,11 @@ public class SavedService {
         return savedPostMapper.countByExample(savedPostExample) >= 1;
     }
 
-    public boolean saveThread(String uid, int threadId) {
+    public boolean saveThread(String uid, Integer threadId) {
+        String key = savedThreadKey(uid);
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(key))) {
+            redisTemplate.opsForSet().add(key, threadId.toString());
+        }
         return savedThreadMapper.insertSelective(new SavedThread()
                 .withCreateAt(new Date())
                 .withThreadId(threadId)
@@ -49,6 +79,10 @@ public class SavedService {
     }
 
     public boolean removeSavedThread(String uid, Integer threadId) {
+        String key = savedThreadKey(uid);
+        if (Boolean.TRUE.equals(redisTemplate.hasKey(key))) {
+            redisTemplate.opsForSet().remove(key, threadId.toString());
+        }
         SavedThreadExample savedThreadExample = new SavedThreadExample();
         savedThreadExample.createCriteria().andUidEqualTo(uid).andThreadIdEqualTo(threadId);
         return savedThreadMapper.deleteByExample(savedThreadExample) == 1;
