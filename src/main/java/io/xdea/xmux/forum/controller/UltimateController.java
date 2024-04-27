383d48bcc05fc68bc7b744c79e992e27c61a9545
Flyingblu <realflyingblu@gmail.com>
2024-04-27 11:24:47 +0800

    Upgrade dependencies

--- src/main/java/io/xdea/xmux/forum/controller/UltimateController.java
+++ src/main/java/io/xdea/xmux/forum/controller/UltimateController.java
@@ -1,6 +1,6 @@
 package io.xdea.xmux.forum.controller;
 
-import io.sentry.spring.tracing.SentryTransaction;
+import io.sentry.spring.jakarta.tracing.SentryTransaction;
 import io.xdea.xmux.forum.service.*;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.springframework.beans.factory.annotation.Autowired;
